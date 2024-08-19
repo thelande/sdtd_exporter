791df1e4330a921c4d72d77d76885111ba0cb56c
Thomas Helander <thomas.helander@gmail.com>
2024-08-19 13:32:17 -0700

    Add initial metrics

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ import (
 	kingpin "github.com/alecthomas/kingpin/v2"
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
+	sdtdclient "github.com/thelande/sdtd_client/pkg/sdtd_client"
 	"github.com/thelande/sdtd_exporter/pkg/collector"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -40,11 +41,26 @@ const (
 )
 
 var (
+	apiUrl = kingpin.Flag(
+		"server.url",
+		"The base URL of the 7 Days to Die dedicated server web API (e.g., http://127.0.0.1:8080).",
+	).Default("http://127.0.0.1:8080").Envar("SDTD_API_URL").String()
+
+	tokenName = kingpin.Flag(
+		"server.token-name",
+		"The name of the API token to use to authenticate with the web server.",
+	).Envar("SDTD_TOKEN_NAME").String()
+
+	tokenSecret = kingpin.Flag(
+		"server.token-secret",
+		"The secret of the API token to use to authenticate with the web server.",
+	).Envar("SDTD_TOKEN_SECRET").String()
+
 	metricsPath = kingpin.Flag(
 		"web.telemetry-path",
 		"Path under which to expose metrics.",
 	).Default("/metrics").String()
-	webConfig = webflag.AddFlags(kingpin.CommandLine, ":9810")
+	webConfig = webflag.AddFlags(kingpin.CommandLine, ":9816")
 	logger    log.Logger
 )
 
@@ -60,7 +76,19 @@ func main() {
 	level.Info(logger).Log("msg", fmt.Sprintf("Starting %s", exporterName), "version", version.Info())
 	level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
 
-	collector := collector.Collector{}
+	client, err := sdtdclient.NewSDTDClient(
+		*apiUrl,
+		&sdtdclient.SDTDAuth{
+			TokenName:   *tokenName,
+			TokenSecret: *tokenSecret,
+		},
+		true,
+		&logger,
+	)
+	if err != nil {
+		panic(err)
+	}
+	collector := collector.Collector{Client: client, Logger: &logger}
 
 	// Uncomment the following two lines and comment out prometheus.MustRegister(collector)
 	// to exclude the go metrics. Make sure to swap line 88 and 89 as well.
--- pkg/collector/collector.go
+++ pkg/collector/collector.go
@@ -1,31 +1,76 @@
 package collector
 
-import "github.com/prometheus/client_golang/prometheus"
+import (
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/prometheus/client_golang/prometheus"
 
-const namespace = "tmpl"
-
-var (
-	upDesc = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, "", "up"),
-		"Was the exporter able to contact the thing?",
-		nil,
-		nil,
-	)
+	sdtdclient "github.com/thelande/sdtd_client/pkg/sdtd_client"
 )
 
-type Collector struct{}
+const namespace = "sdtd"
+
+func GetMetricDesc(name, unit, desc string) *prometheus.Desc {
+	return prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, name, unit),
+		desc, nil, nil,
+	)
+}
+
+var (
+	upDesc       = GetMetricDesc("", "up", "Is the server reachable?")
+	playersDesc  = GetMetricDesc("player", "count", "The number of online players.")
+	zombiesDesc  = GetMetricDesc("zombie", "count", "The number active zombies.")
+	animalsDesc  = GetMetricDesc("animal", "count", "The number active animals.")
+	gametimeDesc = GetMetricDesc("gametime", "seconds", "The current time in the game.")
+)
+
+type Collector struct {
+	Client *sdtdclient.SDTDClient
+	Logger *log.Logger
+}
 
 func (c Collector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- upDesc
+	for _, desc := range []*prometheus.Desc{
+		upDesc,
+		playersDesc,
+		zombiesDesc,
+		animalsDesc,
+		gametimeDesc,
+	} {
+		ch <- desc
+	}
+}
+
+func (c Collector) SetUp(ch chan<- prometheus.Metric, up bool) {
+	val := 0.0
+	if up {
+		val = 1
+	}
+	ch <- prometheus.MustNewConstMetric(upDesc, prometheus.GaugeValue, val)
 }
 
 func (c Collector) Collect(ch chan<- prometheus.Metric) {
-	var success bool
-	var up float64
-
-	if success {
-		up = 1
+	if err := c.Client.Connect(); err != nil {
+		level.Warn(*c.Logger).Log("msg", "Failed to connect to API server", "err", err)
+		c.SetUp(ch, false)
+		return
 	}
 
-	ch <- prometheus.MustNewConstMetric(upDesc, prometheus.GaugeValue, up)
+	c.SetUp(ch, true)
+
+	stats, err := c.Client.GetServerStats()
+	if err != nil {
+		level.Warn(*c.Logger).Log("msg", "Failed to get server stats", "err", err)
+	} else {
+		ch <- prometheus.MustNewConstMetric(playersDesc, prometheus.GaugeValue, float64(stats.Data.Players))
+		ch <- prometheus.MustNewConstMetric(zombiesDesc, prometheus.GaugeValue, float64(stats.Data.Hostiles))
+		ch <- prometheus.MustNewConstMetric(animalsDesc, prometheus.GaugeValue, float64(stats.Data.Animals))
+
+		var gametime float64
+		gametime += float64(stats.Data.GameTime.Days * 86400)
+		gametime += float64(stats.Data.GameTime.Hours * 3600)
+		gametime += float64(stats.Data.GameTime.Minutes * 60)
+		ch <- prometheus.MustNewConstMetric(gametimeDesc, prometheus.CounterValue, gametime)
+	}
 }
